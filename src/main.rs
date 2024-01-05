bb32e01675ea5fdcc349cedca8b6f0fea525c26c
Isaiah Lemmon <42656433+lemmoi@users.noreply.github.com>
2024-01-04 17:18:57 -0800

    added tracing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use ed25519_dalek::{Signature, Verifier, VerifyingKey, PUBLIC_KEY_LENGTH, SIGNATURE_LENGTH};
-use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
-use serde_json::{Value};
+use lambda_http::{run, service_fn, Body, Error, Request, Response, RequestExt};
+use serde_json::{Value, json};
+use tracing::{instrument, info, warn};
 
 /// This is the main body for the function.
 /// Write your code inside it.
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
-async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+/// 
+// #[instrument(skip(request), fields(req_id = %request.lambda_context().request_id))]
+async fn function_handler(request: Request) -> Result<Response<Body>, Error> {
     // Extract some useful information from the request
+    info!("recieved request");
 
-    if verify(&event).is_err() {
+    if verify(&request).is_err() {
+        warn!("unauthorized request");
         let resp = Response::builder()
             .status(401)
             .header("content-type", "text/html")
@@ -19,17 +24,19 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
         return Ok(resp);
     }
 
-    let body = match event.body() {
+    let body = match request.body() {
         Body::Empty => todo!(),
         Body::Text(text) => text,
         Body::Binary(_) => todo!(),
     };
 
     let parsed_body: Value = serde_json::from_str(body.as_str())?;
+    info!(%parsed_body);
 
     let msg_type = parsed_body["type"].as_i64().expect("No type found");
 
     if msg_type == 1 {
+        info!("ping event");
         let resp = Response::builder()
             .status(200)
             .header("content-type", "application/json")
@@ -39,17 +46,21 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
         return Ok(resp);
     }
 
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
+    let message = json!({
+        "type": 4,
+        "data": {
+            "tts": false,
+            "content": "Congrats on sending your command!",
+            "embeds": [],
+            "allowed_mentions": { "parse": [] }
+        }
+    }).to_string();
 
     // Return something that implements IntoResponse.
     // It will be serialized to the right response event automatically by the runtime
     let resp = Response::builder()
         .status(200)
-        .header("content-type", "text/html")
+        .header("content-type", "application/json")
         .body(message.into())
         .map_err(Box::new)?;
     Ok(resp)
@@ -88,8 +99,9 @@ fn get_verifying_key() -> VerifyingKey {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    tracing_subscriber::fmt()
+    tracing_subscriber::fmt().json()
         .with_max_level(tracing::Level::INFO)
+        .with_current_span(false)
         // disable printing the name of the module in every log line.
         .with_target(false)
         // disabling time is handy because CloudWatch will add the ingestion time.
